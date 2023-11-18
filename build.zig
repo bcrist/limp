d090032b075405d7b336f26d4b24edad89440413
Ben Crist <github@magicmoremagic.com>
2023-11-18 16:35:04 -0600

    replace submodules with zig package manager

--- build.zig
+++ build.zig
@@ -1,10 +1,11 @@
 const std = @import("std");
 const config = @import("build_config.zig");
-const TempAllocator = @import("pkg/Zig-TempAllocator/temp_allocator.zig");
+const @"Zig-TempAllocator" = @import("Zig-TempAllocator");
+const TempAllocator = @"Zig-TempAllocator".TempAllocator;
 
-pub fn build(b: *std.build.Builder) void {
+pub fn build(b: *std.build.Build) void {
     const target = b.standardTargetOptions(.{});
-    const mode = b.standardReleaseOptions();
+    const mode = b.standardOptimizeOptions();
     const exe_name = if (mode == .Debug) "limp-debug" else "limp";
 
     const config_step = config.ConfigStep.create(b) catch unreachable;
@@ -12,14 +13,20 @@ pub fn build(b: *std.build.Builder) void {
         .name = "config",
         .source = .{ .generated = &config_step.generated_file },
     };
+    _ = config_pkg;
 
-    const exe = b.addExecutable(exe_name, "src/main.zig");
-    exe.setTarget(target);
-    exe.setBuildMode(mode);
-    exe.addPackage(config_pkg);
-    exe.addPackagePath("TempAllocator", "pkg/Zig-TempAllocator/temp_allocator.zig");
-    exe.addPackagePath("sx", "pkg/Zig-SX/sx.zig");
-    exe.linkLibC();
+    const exe = b.addExecutable(exe_name, .{
+        .name = exe_name,
+        .root_source_file = .{ .path = "src/main.zig" },
+      //  .version: ?std.SemanticVersion = null,
+        .target = target,
+        .optimize = mode,
+        .link_libc = true,
+        .single_threaded = true,
+    });
+    //exe.addPackage(config_pkg);
+    exe.addModule("TempAllocator", b.dependency("Zig-TempAllocator").module("TempAllocator"));
+    exe.addModule("sx", b.dependency("Zig-SX").module("sx"));
     exe.addIncludePath("lua/");
     exe.addIncludePath("zlib/");
     var extraSpace = std.fmt.comptimePrint("{}", .{@sizeOf(TempAllocator)});
@@ -52,31 +59,28 @@ pub fn build(b: *std.build.Builder) void {
         "-Wextra",
     };
 
-    inline for (lua_c_files) |c_file| {
+    for (lua_c_files) |c_file| {
         exe.addCSourceFile("lua/" ++ c_file, &c_flags);
     }
 
-    inline for (zlib_c_files) |c_file| {
+    for (zlib_c_files) |c_file| {
         exe.addCSourceFile("zlib/" ++ c_file, &c_flags);
     }
 
-    exe.single_threaded = true;
-
-    exe.install();
-
-    const run_cmd = exe.run();
-    run_cmd.step.dependOn(b.getInstallStep());
+    b.installArtifact(exe);
+    var run = b.addRunArtifact(exe);
+    run.step.dependOn(b.getInstallStep());
+    b.step("run", "run limp").dependOn(&run.step);
     if (b.args) |args| {
-        run_cmd.addArgs(args);
+        run.addArgs(args);
     }
 
-    const run_step = b.step("run", "Run the app");
-    run_step.dependOn(&run_cmd.step);
+    const tests = b.addTest(.{
+        .root_source_file = .{ .path = "src/main.zig" },
+        .target = target,
+        .optimize = mode,
+    });
 
-    const exe_tests = b.addTest("src/main.zig");
-    exe_tests.setTarget(target);
-    exe_tests.setBuildMode(mode);
-
-    const test_step = b.step("test", "Run unit tests");
-    test_step.dependOn(&exe_tests.step);
+    const test_run = b.addRunArtifact(tests);
+    b.step("test", "test limp").dependOn(&test_run.step);
 }
